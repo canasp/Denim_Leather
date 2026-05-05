0b67fd281f4b50729885641070e808be13cbd88d
Carlos Cañas <carloscanas@ioc.cat>
2026-05-05 21:37:24 +0200

    post login

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Query, Form
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
+
+@app.post("/pim")
+def pim():
+    return {"pim": "pam!"}
+
+class FormData(BaseModel):
+    username: str
+    password: str
+
+@app.post("/login/")
+async def login(data: Annotated[FormData, Form()]):
+    return data
