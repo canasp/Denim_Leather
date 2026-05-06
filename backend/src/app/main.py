06c42b920ce44d3ceb55981bc86fa621ee82b62d
Carlos Cañas <carloscanas@ioc.cat>
2026-05-06 20:44:09 +0200

    add user table

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -2,16 +2,72 @@ from fastapi import Depends, FastAPI, HTTPException, Query, Form
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from typing import Annotated
 from pydantic import BaseModel
+from typing import Annotated
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    password: int | None = Field(default=None, index=True)
+    
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
 
-@app.post("/pim")
-def pim():
-    return {"pim": "pam!"}
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.post("/users/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+@app.get("/users/")
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}
 
 class FormData(BaseModel):
     username: str
